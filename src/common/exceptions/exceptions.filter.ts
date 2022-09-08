f1ad6bb3acd3606c3899daf16fc03f1fccf44a32
Fernando Ochoa <ferj8a@gmail.com>
2022-09-08 17:22:59 -0400

    Update exception filter

--- src/common/exceptions/exceptions.filter.ts
+++ src/common/exceptions/exceptions.filter.ts
@@ -1,28 +1,18 @@
-import {
-  Catch,
-  ArgumentsHost,
-  NotFoundException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Catch, ArgumentsHost, NotFoundException } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
 
+/**
+ * Function to intercept desired exceptions for custom logic.
+ *
+ * @param exception Thrown exception
+ * @param host ArgumentsHost
+ */
 @Catch()
 export class ExceptionsFilter extends BaseExceptionFilter {
-  /**
-   * Function to intercept desired exceptions for custom logic.
-   *
-   * @param exception Thrown exception
-   * @param host ArgumentsHost
-   */
-  catch(exception: unknown, host: ArgumentsHost) {
-    const errName = (exception as Error).name;
-
-    if (errName === 'EntityNotFoundError') {
+  catch(exception: unknown, host: ArgumentsHost): void {
+    if (exception instanceof Prisma.NotFoundError) {
       throw new NotFoundException(exception);
-    }
-
-    if (errName === 'QueryFailedError') {
-      throw new BadRequestException(exception);
     }
 
     super.catch(exception, host);
