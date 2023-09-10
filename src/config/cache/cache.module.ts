5b5dd26614af21e190f871aeff678475ac64d1e4
fer8a <ferj8a@gmail.com>
2023-09-10 11:31:02 -0300

    Add async Mongoose module for DB and init the cache module async as well

--- src/config/cache/cache.module.ts
+++ src/config/cache/cache.module.ts
@@ -1,5 +1,5 @@
 import { CacheModule as cacheModule } from '@nestjs/cache-manager';
-import { cacheConfig } from './cache.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 /**
  * Module used to cache http GET responses
@@ -7,4 +7,13 @@ import { cacheConfig } from './cache.config';
  *
  * @returns {DynamicModule} Nest Application DynamicModule
  */
-export const CacheModule = cacheModule.register(cacheConfig);
+export const CacheModule = cacheModule.register(
+  cacheModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      ttl: configService.get('CACHE_TTL', 5000),
+      max: configService.get('CACHE_MAX', 100),
+    }),
+    inject: [ConfigService],
+  }),
+);
---
+++ src/config/db/mongoose/mongoose.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule as mongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    mongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(
+          'MONGODB_URI',
+          'mongodb://localhost/nest',
+        ),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class MongooseModule {}
